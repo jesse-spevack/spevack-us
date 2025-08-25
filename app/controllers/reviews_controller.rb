2ea15737137d0f83a06435f477796e953753da41
Jesse Spevack <jspevack@gmail.com>
2025-08-24 20:23:46 -0600

    Feat: Fix weekly review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,7 @@ class ReviewsController < ApplicationController
   def show
     @child = current_child
     # Parse the week start date in user's timezone
-    base_date = params[:week_start].present? ? Time.zone.parse(params[:week_start]).to_date : Time.zone.today
+    base_date = params[:week_start].present? ? Time.zone.parse(params[:week_start]).to_date : Time.zone.today - 7.days
     @week_start = week_start_date(base_date, :sunday)
     @week_end = week_end_date(base_date, :sunday)
 
